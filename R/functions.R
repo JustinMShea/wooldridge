1b56231483e971ae4f77c1c0b7c2dda6d69d700a
JustinMShea <jshea@mail.roosevelt.edu>
2017-05-07 16:58:31 -0500

    normalizePath added for cross platform compatibility.

--- R/functions.R
+++ R/functions.R
@@ -1,20 +1,24 @@
 
 # function for downloading data
 
-download_data <- function(x, ...) {
+download_data <- function(x, ...) { 
 
  ## Define the path to the data sets on the textbook website
  woolridge_cengage_url_6th  <- "http://academic.cengage.com/resource_uploads/downloads/130527010X_514733.zip"
+ 
  # Define the path to your working directory, where you'd like the data to download to
- download_destination_path <- paste(getwd(), "Woolridge-Data.ZIP", sep ="/")
+ download_destination_path <- normalizePath(paste(getwd(), "Woolridge-Data.ZIP", sep ="/"))
+
  # Download files from the website to your working directory using the above file path 
  download.file(woolridge_cengage_url_6th, download_destination_path, mode = "wb")
+ 
  # Define where you want to unzip files to
- unzip_file_path <- paste(getwd())
+ unzip_file_path <- normalizePath(paste(getwd()))
+ 
  # Unzip the file into a folder
  unzip(zipfile=download_destination_path, exdir=unzip_file_path)
 
-}
+} 
 
 # list files in the data set file, and verify they downloaded.
 list.files("R data sets for 5e") 
