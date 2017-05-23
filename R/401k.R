9ba6f43cb2741cec31defb0d52bb766501551e5d
JustinMShea <jshea@mail.roosevelt.edu>
2017-05-23 01:26:44 -0500

    Documentation maybe done.

--- R/401k.R
+++ R/401k.R
@@ -1,9 +1,13 @@
-"variable" "label"
-"1" "prate" "participation rate, percent"
-"2" "mrate" "401k plan match rate"
-"3" "totpart" "total 401k participants"
-"4" "totelg" "total eligible for 401k plan"
-"5" "age" "age of 401k plan"
-"6" "totemp" "total number of firm employees"
-"7" "sole" "= 1 if 401k is firm's sole plan"
-"8" "ltotemp" "log of totemp"
+#` 401k
+#` @format A data.frame with 1534 rows and 8 variables:
+#` \describe{
+#`   \item{prate}{participation rate, percent}
+#`   \item{mrate}{401k plan match rate}
+#`   \item{totpart}{total 401k participants}
+#`   \item{totelg}{total eligible for 401k plan}
+#`   \item{age}{age of 401k plan}
+#`   \item{totemp}{total number of firm employees}
+#`   \item{sole}{= 1 if 401k is firm's sole plan}
+#`   \item{ltotemp}{log of totemp}
+#` }
+#` @source \url{https://www.cengage.com/cgi-wadsworth/course_products_wp.pl?fid=M20b&product_isbn_issn=9781305270107
