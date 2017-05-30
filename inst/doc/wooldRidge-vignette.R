6e77a78b8dcfc56120c407e4cb505216f1ce123b
JustinMShea <jshea@mail.roosevelt.edu>
2017-05-30 15:11:55 -0500

    seperated model creation from printing diagnostics in last chunk, and added a line notifying the reader of the intent of the last two lines to print.

--- inst/doc/wooldRidge-vignette.R
+++ inst/doc/wooldRidge-vignette.R
@@ -10,6 +10,8 @@ jtrain_1987_nonunion <- jtrain[index,]
 
 ## ------------------------------------------------------------------------
 linear_model <- lm(lscrap ~ hrsemp + lsales + lemploy, data = jtrain_1987_nonunion)
+
+## ------------------------------------------------------------------------
 linear_model$coefficients
 summary(linear_model)$r.squared
 
