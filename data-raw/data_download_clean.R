46fc10b48f1f5cc66e6c5ee46a29907adf26a217
JustinMShea <jshea@mail.roosevelt.edu>
2017-05-30 14:35:11 -0500

    Previously commented out script surrounding `save()`, which was not needed.

--- data-raw/data_download_clean.R
+++ data-raw/data_download_clean.R
@@ -182,16 +182,16 @@ documentation("meapsingle", meapsingle)
 ###########################################################
 
 # Create "data" folder
-#dir.create("data")
+dir.create("data")
 
 # Create list of all datasets (from .Rdata and .r/txt files)
 # loop over list and write to individual files...'save' dynamics require care.
 # Commented out, so they don't execute.
 
-# dataset_list <- c(RData_names, clean_names)
-# for (i in dataset_list) {
-# save(list = i, file = paste0("data/", i, ".RData"), compress = "xz", compression_level = 9)
-# }
+ dataset_list <- c(RData_names, clean_names)
+ for (i in dataset_list) {
+ save(list = i, file = paste0("data/", i, ".RData"), compress = "xz", compression_level = 9)
+ }
   
 #lets test/check the file compression. 
 tools::checkRdaFiles("data")
@@ -227,8 +227,4 @@ devtools::build_vignettes()
 
 # Finally, remove the extra "Data Sets- R" folder
 unlink("Data Sets- R", recursive = TRUE)
-
-
-
-
 
