43a23eab099588acb762481b9836b26bd5c03b6b
JustinMShea <jshea@mail.roosevelt.edu>
2017-12-13 01:51:41 -0600

    update tests

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -1,5 +1,6 @@
 library(tools)
 
+
 data_folder <- paste0(getwd(),"//data")
 
 
@@ -10,20 +11,20 @@ test_that("Do any files exist in /data that are not .RData files?", {
 })
 
 
-#test_that("Are all data files compressed using the 'xz' algorithm?", {
+test_that("Are all data files compressed using the 'xz' algorithm?", {
   
- # expect_equal(tools::checkRdaFiles()$compress, 
-   #            rep("xz", list.files(data_folder, pattern = "\\.RData$")))
-#})
+ expect_equal(tools::checkRdaFiles(data_folder)$compress, 
+              rep("xz", length(list.files(data_folder, pattern = "\\.RData$"))))
+})
 
 
-#test_that("Is the '/data' directory less than 4MB? (CRAN limit is 5mb)", {
+test_that("Is the '/data' directory smaller than 5MB? (CRAN limit is 5mb)", {
   
- # data_size <- sum(file.info(paste(data_folder, list.files(data_folder), sep = "//"))$size)
+ data_size <- sum(file.info(paste(data_folder, list.files(data_folder), sep = "//"))$size)
   
-  #expect_lt(data_size, 4000000)
+  expect_lt(data_size, 5000000)
   
-#})
+})
 
 
 
