d5ce6ce15e6a181709180438d219347a70953165
JustinMShea <jshea@mail.roosevelt.edu>
2017-12-15 22:41:12 -0600

    decomposed data_documentation_compression into two files

--- data-raw/data_documentation_compression.R
+++ data-raw/data_documentation_compression.R
@@ -94,26 +94,3 @@ for (i in dataset_list) {
 # Create datalist
 tools::add_datalist(getwd())
 
-
-######################################
-## Compute efficiency of compression #
-######################################
-
-data_folder <-  "R data sets for 5e"
-
-# Lets find out how big the original datasets are
-original_size <- sum(file.info(paste(data_folder, list.files(data_folder), sep = "//"))$size)
-original_size # 3,336,438
-
-# Explore size, and order by size.
-file_info <- file.info(paste(data_folder, list.files(data_folder), sep = "//"))
-file_info[order(file_info$size, decreasing = TRUE),]
-
-# Lets find out how big the compressed datasets are.
-new_size <- sum(file.info(paste("data", list.files("data"), sep = "//"))$size)
-new_size # 2,092,800
-
-# compare with original size, calculate percentage change...whoa 38% less!
-original_size <- sum(file.info(paste(data_folder, list.files(data_folder), sep = "//"))$size)
-compression_efficiency <- new_size/original_size * 100 
-compression_efficiency
